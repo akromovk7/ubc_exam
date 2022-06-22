44a3e1ec50aa64ff42260889c6b1ece53c1a8f90
akromovkamoliddin <akromovkwork@gmail.com>
2022-06-22 23:27:28 +0500

    OTAC Number Page Finished

--- lib/screens/home/view/pages/auth/view/pages/otac_number.dart
+++ lib/screens/home/view/pages/auth/view/pages/otac_number.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ubc_app/core/components/text_styles.dart';
+import 'package:ubc_app/core/constants/color_const.dart';
+import 'package:ubc_app/core/constants/icon_const.dart';
+import 'package:ubc_app/screens/home/view/pages/auth/cubit/auth_cubit.dart';
+import 'package:ubc_app/screens/home/view/pages/auth/state/auth_state.dart';
+import 'package:ubc_app/widgets/app_bar_widget.dart';
+import 'package:ubc_app/widgets/button_widget.dart';
+import 'package:ubc_app/widgets/text_form_widget.dart';
+import 'package:ubc_app/widgets/validator_control_widget.dart';
 
 class OTACNumberView extends StatelessWidget {
-  const OTACNumberView({Key? key}) : super(key: key);
+  OTACNumberView({Key? key}) : super(key: key);
+
+  TextEditingController digitCodeController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: ConsColors.kWhite,
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Column(
+          children: [
+            AppBarWidget(
+              leading: ConsIcons.leftArrow,
+              center: Text("OTAC Number", style: FontStyles.appBartext),
+              trailing: SizedBox(
+                width: 160.w,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 48.h),
+              child:
+                  Text("Enter Authorization Code", style: FontStyles.welcome),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 16.h),
+              child:
+                  Text("We have sent SMS to:", style: FontStyles.greytextmini),
+            ),
+            Text("+1 (XXX) XXX-X120", style: FontStyles.appBartext),
+            Container(
+              alignment: Alignment.center,
+              height: 52.h,
+              width: 374.w,
+              margin: EdgeInsets.only(left: 20.w, top: 40.h,right: 20.w,bottom: 10),
+              child: MyTextField.textField(
+                isShown: context.watch<AuthCubit>().getShown,
+                text: "6 Digit Code",
+                controller: digitCodeController,
+                validator: ValidatorController.nameValidator,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 246.w),
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  "Resend Code",
+                  style: FontStyles.forgot,
+                ),
+              ),
+            ),
+            Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 20.w, vertical: 40.h),
+                  child: MyElevatedButton.buttonstyle(
+                      ontap: () =>
+                          context.read<AuthCubit>().changeState(ResetPasswordState()),
+                      text: "Sign in"),
+                ),
+          ],
+        ),
+      ),
+    );
   }
 }
